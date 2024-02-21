4baa7ea24230f2eedb3ea06414b97adbaf97260a
QHPaeek <imagefix@foxmail.com>
2024-02-21 21:38:45 +0800

    更新baudrate_tool

--- README.md
+++ README.md
@@ -2,28 +2,31 @@
 
 使用 Arduino + PN532 制作的 Aime 兼容读卡器。
 
-本仓库为Fork仓库，支持更多的MCU以及开发板。（合宙Air001，以及陆续适配的其他型号）   
+本仓库为Fork仓库，支持更多的MCU以及开发板。 
 
 - 支持卡片类型： [FeliCa](https://zh.wikipedia.org/wiki/FeliCa)（Amusement IC、Suica、八达通等）和 [MIFARE](https://zh.wikipedia.org/wiki/MIFARE)（Aime，Banapassport）
 - 逻辑实现是通过对官方读卡器串口数据进行分析猜测出来的，并非逆向，不保证正确实现
 - 通信数据格式参考了 [Segatools](https://github.com/djhackersdev/segatools) 和官方读卡器抓包数据，可在 [Example.txt](doc/Example.txt) 和 [nfc.txt](https://github.com/djhackersdev/segatools/blob/master/doc/nfc.txt) 查看
-- 参考的PCB设计文件请看`/PCB`目录
+- PCB与外壳均开源，请查阅`/PCB`目录
+- 可以通过baudrate_tool轻易的修改内置波特率和LED参数而无需重新刷写固件
 
 ### 使用方法：
 
 1. 按照 [PN532](https://github.com/elechouse/PN532) 的提示安装库
-2. 按照使用方式，短接读卡器的mode-sw跳线，同时将读卡器与电脑中间接好线，并调整 PN532 上的拨码开关
+2. 按照`/PCB` 目录内对应硬件的使用方式，短接读卡器的mode-sw跳线进入下载模式，同时将读卡器与电脑中间接好线，并调整 PN532 上的拨码开关
 3. 上传 [ReaderTest](tools/ReaderTest/ReaderTest.ino) 测试硬件是否工作正常
-4. 若读卡正常，可按照支持列表打开设备管理器设置 COM 端口号并设置好实际需要使用的波特率
+4. 若读卡正常，重新上传[Arduino-Aime-Reader](Arduino-Aime-Reader.ino)，按照支持列表打开设备管理器设置 COM 端口号并设置好实际需要使用的波特率
 5. 按照游戏的波特率更改好游戏配置文件的`high_baudrate`选项，`115200`是`true`，`38400`是`false`
-6. 读卡器默认情况下为`115200`波特率，如果你的游戏仅支持`38400`波特率，需要运行[baudrate_tool](tools/BaudRateTool/baudrate_tool.exe)以更改读卡器上的设置
+6. 读卡器默认情况下为`115200`波特率，如果你的游戏仅支持`38400`波特率（SDHD 60FPS)，需要运行[baudrate_tool](tools/BaudRateTool/baudrate_tool.exe)以更改读卡器上的设置
 7. 如果有使用 [Segatools](https://github.com/djhackersdev/segatools)，参考 [segatools.ini 设置教程](https://github.com/djhackersdev/segatools/blob/master/doc/config/common.md#enable) 关闭 Aime 模拟读卡器
 8. 上传程序打开游戏测试
 
 如果需要自定义 Aime 卡，安装 [MifareClassicTool](https://github.com/ikarus23/MifareClassicTool)，修改 [Aime 卡示例](doc/aime示例.mct) 后写入空白 MIFARE UID/CUID 卡，即可刷卡使用。    
-328P主控的 Arduino Uno等开发板可能需要在游戏主程序连接前给串口以正确的波特率发送 DTR/RTS，需要先打开一次 Arduino 串口监视器再启动主程序。
+
 
 波特率的概念：正确运行读卡器需要游戏，计算机本地端口，以及读卡器三个地方的设置全部一致才可以。游戏内的波特率一般是不可更改的，计算机本地端口的波特率可以在设置管理器里面修改，读卡器的波特率请通过[baudrate_tool](tools/BaudRateTool/baudrate_tool.exe)进行修改。只有三个设置全部一致才可以使用。
+
+
 
 使用CDC虚拟串口连接计算机的读卡器（STM32），或具有自适应波特率功能的读卡器不需要修改读卡器波特率，因为CDC具有无视波特率的特性。
 
@@ -37,8 +40,8 @@
 | SBZV/SDDF | COM10    | FeliCa,MIFARE | 38400               |
 | SDBT      | COM12    | FeliCa,MIFARE | 38400               |
 
-- 如果读卡器没有正常工作，可以运行[baudrate_tool](tools/BaudRateTool/baudrate_tool.exe)以更改读卡器上的波特率
-- 有使用 amdaemon 的，可以参考 config_common.json 内 aime > unit > port 确认端口号
+- 如果读卡器没有正常工作，可以尝试运行[baudrate_tool](tools/BaudRateTool/baudrate_tool.exe)以更改读卡器上的波特率
+- 参考 config_common.json 内 aime > unit > port 确认端口号
 - 如果 `"high_baudrate" : true` 则波特率是`115200`，否则就是`38400`
 - 在 `"high_baudrate" : true` 的情况下，本读卡器程序支持 emoney 功能，端末认证和刷卡支付均正常（需要游戏和服务器支持）
 
@@ -52,14 +55,15 @@
 | NodeMCU-32S                  | ESP32-S + CH340          | 主要适配环境              |
 | Arduino Uno                  | ATmega328P + CH340       | 未实际测试，据反馈不可用        |
 | Air001 Dev Chip              | Air001 + CH340           | CH340通讯，可能需要修改内部波特率 |
-| STM32  BulePill              | STM32F103C6T6            | 使用CDC链接,不需要修改波特率    |
+| STM32  BulePill              | STM32F103C6T6/C8T6/CBT6  | 使用CDC链接,不需要修改波特率    |
+| STM32  F072                  | STM32F072C8T6            | 使用CDC链接,不需要修改波特率    |
+| Raspberry Pi Pico            | Raspberry RP2040         | 使用CDC链接,不需要修改波特率    |
 
 ### 已知问题：
 
 - 在 NDA_08 命令的写入 Felica 操作没有实现，因为未确认是否会影响卡片后续使用
 - 未确定`res.status`错误码的定义，因此`res.status`的值可能是错误的
 - 因为 PN532 库不支持同时读取多张卡片，所以未实现`mifare_select_tag`，只会读到最先识别的卡片
-- Air001读取Felica的测试待验证
 
 ### 引用库：
 
--- tools/BaudRateTool/baudrate_tool.c
+++ tools/BaudRateTool/baudrate_tool.c
@@ -1,105 +1,496 @@
-#include <stdio.h>  
-#include <windows.h>  
-#include <conio.h>  // 为了使用getch()函数  
-  
-#define SERIAL_PORT_NAME "COM4"  
-#define BAUD_RATE_HIGH 115200
-#define BAUD_RATE_LOW 38400  
-#define TIMEOUT 5000  
-#define WRITE_BUFFER_SIZE 1  
-#define READ_BUFFER_SIZE 1024  
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <windows.h>
+#include <conio.h>
 
-int main() {  
-    HANDLE hSerial;  
-    DCB dcbSerialParams = {0};  
-    COMMTIMEOUTS timeouts = {0};  
-    char writeBuffer[WRITE_BUFFER_SIZE] = {0xfff6};  
-    char readBuffer[READ_BUFFER_SIZE];  
-    DWORD bytesRead;  
-    int success;  
-    int baudRateChoice;
-    int BAUD_RATE;
-    
-   do {  
-        printf("请按1选择115200波特率，按2选择38400波特率：\n");  
-        _getch();  // 等待用户按键，但不显示到控制台  
-        baudRateChoice = _getch() - '0';  // 获取用户输入的数字并转换为整数  
-    } while (baudRateChoice != 1 && baudRateChoice != 2);  
-  
-    if (baudRateChoice == 1) {  
-        BAUD_RATE = BAUD_RATE_HIGH;  // 设置波特率为115200  
-    } else {  
-        BAUD_RATE = BAUD_RATE_LOW;  // 设置波特率为38400  
+// 定义一些常量
+#define PORT_NAME "COM4" // 读卡器的端口号
+#define HIGH_BAUDRATE 115200 // 高波特率
+#define LOW_BAUDRATE 38400 // 低波特率
+#define DATA_LENGTH 12 // 回复数据的长度
+#define CHECKSUM_OFFSET 0xE0 // 校验位的偏移量
+#define LED_MIN 2 // LED亮度的最小值
+#define LED_MAX 255 // LED亮度的最大值
+
+// 定义一些全局变量
+HANDLE hPort; // 串口句柄
+DCB dcb; // 串口参数结构体
+COMMTIMEOUTS timeouts; // 串口超时结构体
+uint8_t send_buffer[8] = {0xE0, 0x06, 0x00, 0x00, 0xF6, 0x00, 0x00, 0xFC}; // 发送数据缓冲区
+uint8_t recv_buffer[DATA_LENGTH]; // 接收数据缓冲区
+uint8_t system_setting_buffer[2] = {0}; // 系统设置缓冲区
+BOOL high_baudrate_mode = FALSE; // 高波特率模式标志
+uint8_t change_highbaudrate_mode = 0;
+BOOL led_enabled = FALSE; // LED启用标志
+uint8_t led_brightness = 0; // LED亮度
+uint8_t firmware_version = 0; // 固件版本号
+uint8_t hardware_version = 0; // 硬件版本号
+
+// 打开串口的函数，返回值为BOOL，表示是否成功
+BOOL open_port()
+{
+    // 打开串口
+    hPort = CreateFile(PORT_NAME, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+    if (hPort == INVALID_HANDLE_VALUE)
+    {
+        printf("无法打开串口%s！\n", PORT_NAME);
+        return FALSE;
     }
-    // 打开串行端口  
-    hSerial = CreateFile(SERIAL_PORT_NAME, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);  
-    if (hSerial == INVALID_HANDLE_VALUE) {  
-        printf("扫描不到读卡器，请确认读卡器已连接并且分配到端口COM4\n");  
-	printf("按任意键退出......");
-        getch();  // 等待用户按键退出  
-        return 1;  
-    }  
-  
-    // 设置串行端口参数  
-    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);  
-    if (!GetCommState(hSerial, &dcbSerialParams)) {  
-        printf("无法获取串行端口当前状态\n");  
-	printf("按任意键退出......");
-        getch();  // 等待用户按键退出  
-        CloseHandle(hSerial);  
-        return 1;  
-    }  
-    dcbSerialParams.BaudRate = BAUD_RATE;  
-    dcbSerialParams.ByteSize = 8;  
-    dcbSerialParams.StopBits = ONESTOPBIT;  
-    dcbSerialParams.Parity   = NOPARITY;  
-    if (!SetCommState(hSerial, &dcbSerialParams)) {  
-        printf("无法设置串行端口状态\n");  
-	printf("按任意键退出......");
-        getch();  // 等待用户按键退出  
-        CloseHandle(hSerial);  
-        return 1;  
-    }  
-  
-    // 设置串行端口超时参数  
-    timeouts.ReadIntervalTimeout = MAXDWORD;  
-    timeouts.ReadTotalTimeoutConstant = 0;  
-    timeouts.ReadTotalTimeoutMultiplier = 0;  
-    timeouts.WriteTotalTimeoutConstant = 0;  
-    timeouts.WriteTotalTimeoutMultiplier = TIMEOUT;  
-    if (!SetCommTimeouts(hSerial, &timeouts)) {  
-        printf("无法设置串行端口超时参数\n");  
-	printf("按任意键退出......");
-        getch();  // 等待用户按键退出  
-        CloseHandle(hSerial);  
-        return 1;  
-    }  
-  
-    // 向串行端口写入数据并检查返回值  
-    success = WriteFile(hSerial, writeBuffer, WRITE_BUFFER_SIZE, &bytesRead, NULL);  
-    if (success == 0) {  
-        printf("发送指令失败\n");  
-	printf("按任意键退出......");
-        getch();  // 等待用户按键退出  
-        CloseHandle(hSerial);  
-        return 1;  
-    } else {  
-        printf("发送指令成功\n");  
-    }  
-  
-    // 从串行端口读取数据并检查返回值和数据内容  
-    success = ReadFile(hSerial, readBuffer, READ_BUFFER_SIZE, &bytesRead, NULL);  
-    if (success == 0) {  
-        printf("读取数据失败\n");  
-        getch();  // 等待用户按键退出  
-        CloseHandle(hSerial);  
-        return 1;  
-    } else {  
-        printf("读取数据成功\n");  
-        if (bytesRead == 2 && readBuffer[0] == 0xff && readBuffer[1] == 0x99 && readBuffer[2] == 0xaa) {  
-            printf("返回的数据是0xff99aa，操作成功\n");  
-        } else {  
-            printf("返回的数据不是0xff99aa，操作失败\n");  
-        }  
-	getch();  // 等待用户按键退出，然后关闭串行端口
-}}
+
+    // 获取串口参数
+    if (!GetCommState(hPort, &dcb))
+    {
+        printf("无法获取串口%s的参数！\n", PORT_NAME);
+        CloseHandle(hPort);
+        return FALSE;
+    }
+
+    // 设置串口参数
+    dcb.BaudRate = HIGH_BAUDRATE; // 设置波特率为高波特率
+    dcb.ByteSize = 8; // 设置数据位为8
+    dcb.Parity = NOPARITY; // 设置无奇偶校验
+    dcb.StopBits = ONESTOPBIT; // 设置停止位为1
+    if (!SetCommState(hPort, &dcb))
+    {
+        printf("无法设置串口%s的参数！\n", PORT_NAME);
+        CloseHandle(hPort);
+        return FALSE;
+    }
+
+    // 获取串口超时
+    if (!GetCommTimeouts(hPort, &timeouts))
+    {
+        printf("无法获取串口%s的超时！\n", PORT_NAME);
+        CloseHandle(hPort);
+        return FALSE;
+    }
+
+    // 设置串口超时
+    timeouts.ReadIntervalTimeout = 50; // 设置读取间隔超时为50毫秒
+    timeouts.ReadTotalTimeoutConstant = 1000; // 设置读取总超时常量为1000毫秒
+    timeouts.ReadTotalTimeoutMultiplier = 10; // 设置读取总超时乘数为10毫秒
+    timeouts.WriteTotalTimeoutConstant = 1000; // 设置写入总超时常量为1000毫秒
+    timeouts.WriteTotalTimeoutMultiplier = 10; // 设置写入总超时乘数为10毫秒
+    if (!SetCommTimeouts(hPort, &timeouts))
+    {
+        printf("无法设置串口%s的超时！\n", PORT_NAME);
+        CloseHandle(hPort);
+        return FALSE;
+    }
+
+    // 返回成功
+    return TRUE;
+}
+
+// 关闭串口的函数，无返回值
+void close_port()
+{
+    // 关闭串口
+    CloseHandle(hPort);
+}
+
+// 发送数据的函数，参数为数据长度，返回值为BOOL，表示是否成功
+BOOL send_data(int length)
+{
+    DWORD bytes_written; // 写入的字节数
+    // 写入数据
+    if (!WriteFile(hPort, send_buffer, length, &bytes_written, NULL))
+    {
+        printf("无法向串口%s写入数据！\n", PORT_NAME);
+        return FALSE;
+    }
+    // 检查写入的字节数是否正确
+    if (bytes_written != length)
+    {
+        printf("向串口%s写入数据不完整！\n", PORT_NAME);
+        return FALSE;
+    }
+    // 返回成功
+    return TRUE;
+}
+
+// 接收数据的函数，参数为数据长度，返回值为BOOL，表示是否成功
+BOOL recv_data(int length)
+{
+    DWORD bytes_read; // 读取的字节数
+    // 读取数据
+    if (!ReadFile(hPort, recv_buffer, length, &bytes_read, NULL))
+    {
+        printf("无法从串口%s读取数据！\n", PORT_NAME);
+        return FALSE;
+    }
+    // 检查读取的字节数是否正确
+    if (bytes_read != length)
+    {
+        //printf("从串口%s读取数据不完整！\n", PORT_NAME);
+        return FALSE;
+    }
+    // 返回成功
+    return TRUE;
+}
+
+// 处理数据的函数，参数为数据长度，返回值为BOOL，表示是否成功
+BOOL process_data(int length)
+{
+    int i; // 循环变量
+    uint8_t checksum = 0; // 校验位
+    // 检查第一个字节是否为E0
+    if (recv_buffer[0] != 0xE0)
+    {
+        printf("读卡器连接错误！请检查连接！\n");
+        return FALSE;
+    }
+    // 计算校验位
+    for (i = 0; i < length - 1; i++)
+    {
+        checksum += recv_buffer[i];
+    }
+    checksum -= CHECKSUM_OFFSET;
+    // 检查校验位是否正确
+    if (checksum != recv_buffer[length - 1])
+    {
+        printf("读卡器连接错误！请检查连接！\n");
+        return FALSE;
+    }
+    // 解析数据
+    high_baudrate_mode = recv_buffer[7] & 0x02; // 第8个字节的第2位表示高波特率模式
+    led_enabled = recv_buffer[7] & 0x04; // 第8个字节的第3位表示LED启用
+    led_brightness = recv_buffer[8]; // 第9个字节表示LED亮度
+    firmware_version = recv_buffer[9]; // 第10个字节表示固件版本号
+    hardware_version = recv_buffer[10]; // 第11个字节表示硬件版本号
+    // 返回成功
+    return TRUE;
+}
+
+// 打印数据的函数，无参数，无返回值
+void print_data()
+{
+    // 打印数据
+    printf("读卡器状态信息如下：\n");
+    printf("高波特率模式：%s\n", high_baudrate_mode ? "是" : "否");
+    printf("LED启用：%s\n", led_enabled ? "是" : "否");
+    printf("LED亮度：%d\n", led_brightness);
+    printf("固件版本：%d\n", firmware_version);
+	switch(hardware_version)
+	{
+		case 1:
+			printf("硬件版本：ATmega32U4\n");
+			break;
+		case 2:
+			printf("硬件版本：SAMD_ZERO\n");
+			break;
+		case 3:
+			printf("硬件版本：ESP8266\n");
+			break;
+		case 4:
+			printf("硬件版本：ESP32\n");
+			break;
+		case 5:
+			printf("硬件版本：AIR001/PY32F002\n");
+			break;
+		case 6:
+			printf("硬件版本：STM32F1\n");
+			break;
+		case 7:
+			printf("硬件版本：STM32F0\n");
+			break;
+		case 8:
+			printf("硬件版本：RP2040\n");
+			break;
+		case 9:
+			printf("硬件版本：ATmega328P\n");
+			break;
+		default:
+			printf("硬件版本：未知\n");
+			break;
+	}	
+}
+
+// 修改波特率的函数，参数为波特率，返回值为BOOL，表示是否成功
+BOOL change_baudrate(int baudrate)
+{
+    // 获取串口参数
+    if (!GetCommState(hPort, &dcb))
+    {
+        printf("无法获取串口%s的参数！\n", PORT_NAME);
+        return FALSE;
+    }
+    // 设置串口参数
+    dcb.BaudRate = baudrate; // 设置波特率
+    if (!SetCommState(    hPort, &dcb))
+    {
+        printf("无法设置串口%s的参数！\n", PORT_NAME);
+        return FALSE;
+    }
+    // 返回成功
+    return TRUE;
+}
+
+// 获取用户输入的函数，参数为提示信息，返回值为char，表示用户输入的字符
+char get_user_input(char *prompt)
+{
+    char input; // 用户输入的字符
+    // 打印提示信息
+    printf("%s", prompt);
+    // 获取用户输入
+    input = getch();
+    // 返回用户输入
+    return input;
+}
+
+// 获取用户输入的数字的函数，参数为提示信息，返回值为int，表示用户输入的数字
+int get_user_input_number(char *prompt)
+{
+    char input[256]; // 用户输入的字符串
+    int number; // 用户输入的数字
+    // 打印提示信息
+    printf("%s", prompt);
+    // 获取用户输入
+    fgets(input, 256, stdin);
+    // 尝试将输入转换为数字
+    if (sscanf(input, "%d", &number) != 1)
+    {
+        // 转换失败，返回-1
+        return -1;
+    }
+    // 转换成功，返回数字
+    return number;
+}
+
+// 主函数
+int main()
+{
+    // 打印语句”请将读卡器的端口号改为COM4，按任意键继续“
+printf("本工具用于修改Aime读卡器内部设置，源代码见https://github.com/QHPaeek/Arduino-Aime-Reader\n");
+printf("读卡器EEPROM寿命有限，请不要频繁修改！\n");
+    printf("请将读卡器的端口号改为COM4，按任意键继续\n");
+    // 等待用户按下任意按键
+    getch();
+    // 打开串口
+    if (!open_port())
+    {
+        // 打开失败，等待用户按下任意键退出程序
+        getch();
+        return -1;
+    }
+    // 向COM4端口以115200波特率输出16进制数据E0 06 00 00 F6 00 00 FC，并且监听回复。
+    if (!send_data(8))
+    {
+        // 发送失败，关闭串口，等待用户按下任意键退出程序
+        close_port();
+        getch();
+        return -1;
+    }
+    // 接收数据
+    if (!recv_data(DATA_LENGTH))
+    {
+        // 接收失败，尝试以38400波特率重新输出上述数据，并且监听回复。
+        if (!change_baudrate(LOW_BAUDRATE))
+        {
+            // 修改波特率失败，关闭串口，等待用户按下任意键退出程序
+            close_port();
+            getch();
+            return -1;
+        }
+        if (!send_data(8))
+        {
+            // 发送失败，关闭串口，等待用户按下任意键退出程序
+            close_port();
+            getch();
+            return -1;
+        }
+        if (!recv_data(DATA_LENGTH))
+        {
+            // 接收失败，打印语句”读卡器连接错误！请检查连接！“，关闭串口，等待用户按下任意键退出程序
+            printf("读卡器连接错误！请检查连接！\n");
+            close_port();
+            getch();
+            return -1;
+        }
+    }
+    // 处理数据
+    if (!process_data(DATA_LENGTH))
+    {
+        // 处理失败，关闭串口，等待用户按下任意键退出程序
+        close_port();
+        getch();
+        return -1;
+    }
+    // 打印数据
+    print_data();
+    // 打印语句”否修改读卡器设置？输入y继续，输入n退出“，输入y或者Y则继续，输入n或者N则直接关闭程序，输入其他内容则提示用户”请重新输入！“
+    char choice; // 用户的选择
+    while (1)
+    {
+        // 获取用户输入
+        choice = get_user_input("是否修改读卡器设置？输入y继续，输入n退出\n");
+        // 判断用户输入
+        if (choice == 'y' || choice == 'Y')
+        {
+            // 输入y或者Y，跳出循环，继续程序
+            break;
+        }
+        else if (choice == 'n' || choice == 'N')
+        {
+            // 输入n或者N，关闭串口，退出程序
+            close_port();
+            return 0;
+        }
+        else
+        {
+            // 输入其他内容，提示用户”请重新输入！“
+            printf("请重新输入！\n");
+        }
+    }
+    // 如果用户选择继续，则先根据前面读到的设置中”读卡器是否工作在高波特率模式“选择电脑端口的发送波特率，是则为115200，否则为38400。
+    if (high_baudrate_mode)
+    {
+        // 高波特率模式，设置波特率为115200
+        if (!change_baudrate(HIGH_BAUDRATE))
+        {
+            // 修改波特率失败，关闭串口，等待用户按下任意键退出程序
+            close_port();
+            getch();
+            return -1;
+        }
+    }
+    else
+    {
+        // 低波特率模式，设置波特率为38400
+        if (!change_baudrate(LOW_BAUDRATE))
+        {
+            // 修改波特率失败，关闭串口，等待用户按下任意键退出程序
+            close_port();
+            getch();
+            return -1;
+        }
+    }
+    // 创建一个uint8_t system_setting_buffer[2] = {0}，打印语句”是否启用高波特率模式？输入y启用，输入n不启用“，用户输入y则执行system_setting_buffer[0] 的第二位置1。用户输入其他内容则提示”请重新输入！“
+    while (1)
+    {
+        // 获取用户输入
+        choice = get_user_input("是否启用高波特率模式？输入y启用，输入n不启用\n");
+        // 判断用户输入
+        if (choice == 'y' || choice == 'Y')
+        {
+            // 输入y或者Y，设置system_setting_buffer[0] 的第二位置1，跳出循环
+            system_setting_buffer[0] |= 0x02;
+		change_highbaudrate_mode = 1;
+            break;
+        }
+        else if (choice == 'n' || choice == 'N')
+        {
+            // 输入n或者N，跳出循环
+		change_highbaudrate_mode = 0;
+            break;
+        }
+        else
+        {
+            // 输入其他内容，提示用户”请重新输入！“
+            printf("请重新输入！\n");
+        }
+    }
+    // 打印语句”是否启用LED？输入y启用，输入n不启用“，用户输入y则执行system_setting_buffer[0] 的第三位置1。用户输入其他内容则提示”请重新输入！“
+    while (1)
+    {
+        // 获取用户输入
+        choice = get_user_input("是否启用LED？输入y启用，输入n不启用\n");
+        // 判断用户输入
+        if (choice == 'y' || choice == 'Y')
+        {
+            // 输入y或者Y，设置system_setting_buffer[0] 的第三位置1，跳出循环
+            system_setting_buffer[0] |= 0x04;
+            break;
+        }
+        else if (choice == 'n' || choice == 'N')
+        {
+            // 输入n或者N，跳出循环
+            break;
+        }
+        else
+        {
+            // 输入其他内容，提示用户”请重新输入！“
+            printf("请重新输入！\n");
+        }
+    }
+    // 打印语句”请输入LED亮度范围，范围为0~255，按回车继续“，等待用户输入数字并按下回车，如果用户输入的不是数字或者范围超过了0~255，则提示用户”请重新输入!“，如果符合要求则将输入的数值赋system_setting_buffer[1]
+    int brightness; // LED亮度
+    while (1)
+    {
+        // 获取用户输入的数字
+        brightness = get_user_input_number("请输入LED亮度范围，范围为0~255，按回车继续\n");
+        // 判断用户输入的数字是否合法
+        if (brightness >= 0 && brightness <= 255)
+        {
+            // 合法，将输入的数值赋给system_setting_buffer[1]，跳出循环
+            system_setting_buffer[1] = brightness;
+            break;
+        }
+        else
+        {
+            // 不合法，提示用户”请重新输入!“
+            printf("请重新输入!\n");
+        }
+    }
+if(high_baudrate_mode)
+{
+	change_baudrate(115200);
+}
+else
+{
+	change_baudrate(38400);
+}
+    // 向COM4端口，以第7步中决定的波特率，先发送16进制数据{E0 08 00 00 F7 02}，再发送system_setting_buffer[2]，再发送16进制数据{0}，最后计算8+0xf7+2+system_setting_buffer[0]+system_setting_buffer[1]，并且将结果发送出去。
+    send_buffer[0] = 0xE0; // 第一个字节为E0
+    send_buffer[1] = 0x08; // 第二个字节为8
+    send_buffer[2] = 0x00; // 第三个字节为0
+    send_buffer[3] = 0x00; // 第四个字节为0
+    send_buffer[4] = 0xF7; // 第五个字节为F7
+    send_buffer[5] = 0x02; // 第六个字节为2
+    send_buffer[6] = system_setting_buffer[0]; // 第七个字节为system_setting_buffer[0]
+    send_buffer[7] = system_setting_buffer[1]; // 第八个字节为system_setting_buffer[1]
+    send_buffer[8] = 0x00; // 第九个字节为0
+    send_buffer[9] = 8 + 0xF7 + 2 + system_setting_buffer[0] + system_setting_buffer[1]; // 第十个字节为校验位
+	DWORD bytes_written;
+    WriteFile(hPort, send_buffer,10, &bytes_written, NULL);
+Sleep(4);
+    // 改变串口波特率，如果第8步中用户选择了y，那么将串口的波特率改变为115200，否则改变为38400
+    if (change_highbaudrate_mode)
+    {
+        // 用户选择了高波特率模式，设置波特率为115200
+        change_baudrate(HIGH_BAUDRATE);
+    }
+    else
+    {
+        // 用户没有选择高波特率模式，设置波特率为38400
+	change_baudrate(LOW_BAUDRATE);
+    }
+    // 监听COM4端口，如果收到回复{E0 06 00 00 F7 00 00 FD}，则提示用户”修改成功！按任意键退出“，等待用户按下任意键退出程序。如果没有收到回复，或者回复的第一个字节不是E0，则提示”修改失败！按任意键退出“，等待用户按下任意键退出程序。
+    if (!recv_data(8))
+    {
+        // 接收失败，提示”修改失败！按任意键退出“，关闭串口，等待用户按下任意键退出程序
+        printf("修改失败！按任意键退出\n");
+        close_port();
+        getch();
+        return -1;
+    }
+    // 检查回复是否正确
+    if (recv_buffer[0] == 0xE0 && recv_buffer[1] == 0x06 && recv_buffer[2] == 0x00 && recv_buffer[3] == 0x00 && recv_buffer[4] == 0xF7 && recv_buffer[5] == 0x00 && recv_buffer[6] == 0x00 && recv_buffer[7] == 0xFD)
+    {
+        // 回复正确，提示”修改成功！按任意键退出“，关闭串口，等待用户按下任意键退出程序
+        printf("修改成功！按任意键退出\n");
+        close_port();
+        getch();
+        return 0;
+    }
+    else
+    {
+        // 回复错误，提示”修改失败！按任意键退出“，关闭串口，等待用户按下任意键退出程序
+        printf("修改失败！按任意键退出\n");
+        close_port();
+        getch();
+        return -1;
+    }
+}
